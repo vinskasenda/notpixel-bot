600a4db4c22b9f2b6481fd36bc80162ce8d79abb
Vikitoshi <31087501+vinskasenda@users.noreply.github.com>
2024-10-16 09:29:39 +0700

    Update twist.js

--- app/src/utils/twist.js
+++ app/src/utils/twist.js
@@ -1,32 +1,32 @@
 import { Twisters } from 'twisters';
-import a6_0x54d76a from './logger.js';
+import a6_0x27666d from './logger.js';
 import { Core } from '../core/core.js';
 class Twist {
   constructor() {
     this.twisters = new Twisters();
   }
-  ["log"](_0x4366fe = '', _0x432616 = '', _0xe10c00 = new Core(), _0x1cfa93) {
-    if (_0x1cfa93 == undefined) {
-      a6_0x54d76a.info(_0x432616.id + " - " + _0x4366fe);
-      _0x1cfa93 = '-';
+  ["log"](_0x546d8f = '', _0x5e586a = '', _0x45cdc3 = new Core(), _0x22765b) {
+    if (_0x22765b == undefined) {
+      a6_0x27666d.info(_0x5e586a.id + " - " + _0x546d8f);
+      _0x22765b = '-';
     }
-    const _0x162910 = _0xe10c00.mining ?? {};
-    const _0x1df5fc = _0x162910.userBalance ?? '-';
-    const _0x346c3e = _0x162910.charges ?? '-';
-    this.twisters.put(_0x432616.id, {
-      'text': "\n================= Account " + _0x432616.id + " =============\nName         : " + (_0x432616.firstName ?? 'Unamed') + " " + (_0x432616.lastName ?? '') + " \nBalance      : " + _0x1df5fc + "\nCharge       : " + _0x346c3e + "\n\nStatus : " + _0x4366fe + "\nDelay : " + _0x1cfa93 + "\n=============================================="
+    const _0x55416c = _0x45cdc3.mining ?? {};
+    const _0x590a69 = _0x55416c.userBalance ?? '-';
+    const _0x255a50 = _0x55416c.charges ?? '-';
+    this.twisters.put(_0x5e586a.id, {
+      'text': "\n================= Account " + _0x5e586a.id + " =============\nName         : " + (_0x5e586a.firstName ?? "Unamed") + " " + (_0x5e586a.lastName ?? '') + " \nBalance      : " + _0x590a69 + "\nCharge       : " + _0x255a50 + "\n\nStatus : " + _0x546d8f + "\nDelay : " + _0x22765b + "\n=============================================="
     });
   }
-  ["info"](_0x3227b0 = '') {
+  ["info"](_0x113347 = '') {
     this.twisters.put(0x2, {
-      'text': "\n==============================================\nInfo : " + _0x3227b0 + "\n=============================================="
+      'text': "\n==============================================\nInfo : " + _0x113347 + "\n=============================================="
     });
     return;
   }
   ["clearInfo"]() {
     this.twisters.remove(0x2);
   }
-  async ["clear"](_0x784a20) {
+  async ["clear"](_0x12b76b) {
     await this.twisters.flush();
   }
 }
